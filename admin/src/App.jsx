e9f2db7902c651c11d334e85928d7eb1c3d867f0
knllydgmntz <knllydgmntz@gmail.com>
2024-06-09 12:56:11 +0800

    adding admin panel

---
+++ admin/src/App.jsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Sidebar from "./components/Sidebar/Sidebar.jsx";
+import Add from "./pages/Add/Add.jsx";
+import List from "./pages/List/List.jsx";
+import Orders from "./pages/Orders/Orders.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <div>
+      <ToastContainer />
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
